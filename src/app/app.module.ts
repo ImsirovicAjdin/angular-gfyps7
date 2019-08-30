d5825761a168d35d672151bf3c062069a7e6155e
ImsirovicAjdin <ajdika@live.com>
2019-08-30 15:29:24 +0200

    Add progressbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PopoverModule } from 'ngx-bootstrap/popover';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoModalStaticComponent } from './modals/modals.component.ts';
 import { DemoPaginationPageChangedComponent } from './pagination/pagination.component.ts';
 import { DemoPopoverDynamicComponent } from './popover/popover.component.ts';
+import { DemoProgressbarDynamicComponent } from './progressbar/progressbar.component.ts';
 
 @NgModule({
   imports: [ 
@@ -17,10 +19,12 @@ import { DemoPopoverDynamicComponent } from './popover/popover.component.ts';
     FormsModule,
     ModalModule.forRoot(),
     PaginationModule.forRoot(),
-    PopoverModule.forRoot()
+    PopoverModule.forRoot(),
+    ProgressbarModule.forRoot()
   ],
   declarations: [ AppComponent, HelloComponent, DemoModalStaticComponent,
-    DemoPaginationPageChangedComponent, DemoPopoverDynamicComponent  ],
+    DemoPaginationPageChangedComponent, DemoPopoverDynamicComponent, DemoProgressbarDynamicComponent
+      ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
