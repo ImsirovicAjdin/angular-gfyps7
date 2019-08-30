ac9d90b37e5000cca7ea46da32dd0ded76aaf741
ImsirovicAjdin <ajdika@live.com>
2019-08-30 15:00:26 +0200

    Added a pagination component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoModalStaticComponent } from './modals/modals.component.ts';
+import { DemoPaginationPageChangedComponent } from './pagination/pagination.component.ts';
+
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
-    ModalModule.forRoot() 
+    ModalModule.forRoot(),
+    PaginationModule.forRoot()
   ],
-  declarations: [ AppComponent, HelloComponent, DemoModalStaticComponent ],
+  declarations: [ AppComponent, HelloComponent, DemoModalStaticComponent,
+    DemoPaginationPageChangedComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
