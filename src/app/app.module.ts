ce137e458364ff244819f53b0025262be203f3f3
ImsirovicAjdin <ajdika@live.com>
2019-08-30 15:18:58 +0200

    Added a popover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoModalStaticComponent } from './modals/modals.component.ts';
 import { DemoPaginationPageChangedComponent } from './pagination/pagination.component.ts';
-
+import { DemoPopoverDynamicComponent } from './popover/popover.component.ts';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
     ModalModule.forRoot(),
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    PopoverModule.forRoot()
   ],
   declarations: [ AppComponent, HelloComponent, DemoModalStaticComponent,
-    DemoPaginationPageChangedComponent ],
+    DemoPaginationPageChangedComponent, DemoPopoverDynamicComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
